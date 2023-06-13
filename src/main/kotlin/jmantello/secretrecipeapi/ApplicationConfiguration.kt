1d5152215c20963021ab90e7415cb27cc59a3eac
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-06-13 15:05:51 -0700

    add Auth0 configuration

--- src/main/kotlin/jmantello/secretrecipeapi/ApplicationConfiguration.kt
+++ src/main/kotlin/jmantello/secretrecipeapi/ApplicationConfiguration.kt
@@ -25,29 +25,6 @@ class ApplicationConfiguration {
         user.password = "password"
         userRepository.save(user)
     }
-
-    // Security
-    @Bean
-    fun apiFilterChain(http: HttpSecurity): SecurityFilterChain {
-        val apiPattern = "/api/**"
-        http {
-            securityMatcher(apiPattern)
-            authorizeRequests {
-                authorize(HttpMethod.POST,"/api/auth/register", permitAll)
-                authorize(HttpMethod.POST,"/api/auth/login", permitAll)
-                authorize(HttpMethod.GET,"/api/recipe", permitAll)
-                authorize(HttpMethod.GET,"/api/recipe/*", permitAll)
-                authorize(anyRequest, permitAll)
-            }
-            httpBasic { }
-
-            // Csrf token disabled since api does not support users right now. Will be removed when users are added
-            csrf {
-                ignoringRequestMatchers(apiPattern)
-            }
-        }
-        return http.build()
-    }
 }
 
 @Configuration
---
+++ src/main/kotlin/jmantello/secretrecipeapi/security/SecurityConfig.kt
@@ -0,0 +1,58 @@
+package jmantello.secretrecipeapi.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.*
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+class SecurityConfig {
+
+    @Value("\${auth0.audience}")
+    private val audience: String = String()
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private val issuer: String = String()
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer) as NimbusJwtDecoder
+        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience)
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
+        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
+        jwtDecoder.setJwtValidator(withAudience)
+        return jwtDecoder
+    }
+
+    @Bean
+    fun apiFilterChain(http: HttpSecurity): SecurityFilterChain {
+        val apiPattern = "/api/**"
+        http {
+            securityMatcher(apiPattern)
+            authorizeRequests {
+                authorize(HttpMethod.POST,"/api/auth/register", permitAll)
+                authorize(HttpMethod.POST,"/api/auth/login", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            httpBasic { }
+            oauth2ResourceServer {
+                jwt {  }
+            }
+
+            // Csrf token disabled since api does not support users right now. Will be removed when users are added
+            csrf {
+                ignoringRequestMatchers(apiPattern)
+            }
+        }
+        return http.build()
+    }
+}
